0b6cb4d1661aae671e00eb3d8d1ffe6288197217
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2020-11-09 19:47:40 +0100

    Add router

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import Home from './Home';
+import Anim1 from './anim/bg/Anim1';
+import PageNotFound from './PageNotFound';
+import '../styles/App.css';
 
-const App = () => {
-  return (
-    <div>
-      <h1>HELLO WORLD</h1>
-    </div>
-  )
+class App extends React.Component {
+  render() {
+    const today = new Date();
+    const year = today.getFullYear();
+    return (
+      <div className="app">
+        <BrowserRouter>
+          <div>
+            <Link to="/">Home</Link>
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/1" exact component={Anim1} />
+              <Route component={PageNotFound} />
+            </Switch>
+            <footer>Copyright {year} Justyna Biernacka</footer>
+          </div>
+          </BrowserRouter>
+      </div>
+    )
+  }
 }
 
 export default App;
