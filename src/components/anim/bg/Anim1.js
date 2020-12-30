26251249a9218cb1be4809b49ff0a7a32d22972e
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2020-12-30 18:28:08 +0100

    Add animation 1 background parallax

--- src/components/anim/bg/Anim1.js
+++ src/components/anim/bg/Anim1.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import '../../../styles/Anim1.css';
+
+const parallax = (e) => {
+  const el = document.querySelector('.parallax');
+  if (!(window.innerWidth < 500)) {
+    const w = window.innerWidth/2;
+    const h = window.innerHeight/2;
+    const mouse_w = e.clientX;
+    const mouse_h = e.clientY;
+    const pos1 = `${50 - (mouse_w - w) * 0.08}% ${50 - (mouse_h - h) * 0.08}%`;
+    const pos2 = `${50 - (mouse_w - w) * 0.04}% ${50 - (mouse_h - h) * 0.04}%`;
+    const pos3 = `${50 - (mouse_w - w) * 0.02}% ${50 - (mouse_h - h) * 0.02}%`;
+    const pos4 = `${50 - (mouse_w - w) * 0.01}% ${50 - (mouse_h - h) * 0.01}%`;
+    const final_position = `${pos1}, ${pos2}, ${pos3}, ${pos4}`;
+    console.log(final_position);
+    el.style.backgroundPosition = final_position;
+  } 
+}
 
 class Anim1 extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Animation 1</h1>
-      </div>
+      <div className="parallax" onMouseMove={parallax}></div>
     )
   }
 }
