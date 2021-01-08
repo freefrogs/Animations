2912cacba700e6e514081c44df9ad0a61bb47e28
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2021-01-08 15:18:22 +0100

    Add rwd & touch events

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,11 +4,20 @@ import '../styles/Home.css';
 const n = 150;
 
 const shadow = (e) => {
+  let x;
+  let y;
+
+  if (e.type === 'touchstart' || e.type === 'touchmove') {
+    x = e.touches[0].clientX;
+    y = e.touches[0].clientY
+  } else {
+    x = e.clientX;
+    y = e.clientY;
+  }
   const box = document.querySelector('.home');
   const title = document.querySelector('.home h1');
 
   const { offsetWidth: width, offsetHeight: height } = box;
-  let { clientX: x, clientY: y} = e;
 
   const posX = Math.round((x/width * n) - (n/2));
   const posY = Math.round((y/height * n) - (n/2));
@@ -19,7 +28,7 @@ const shadow = (e) => {
 
 const Home = () => {
   return (
-    <div className="home" onMouseMove={shadow}>
+    <div className="home" onMouseMove={shadow} onTouchStart={shadow} onTouchMove={shadow} >
         <h1>Animations</h1>
     </div>
   )
--- src/components/anim/bg/Anim1.js
+++ src/components/anim/bg/Anim1.js
@@ -3,23 +3,31 @@ import '../../../styles/Anim1.css';
 
 const parallax = (e) => {
   const el = document.querySelector('.parallax');
-  if (!(window.innerWidth < 500)) {
-    const w = window.innerWidth/2;
-    const h = window.innerHeight/2;
-    const mouse_w = e.clientX;
-    const mouse_h = e.clientY;
-    const pos1 = `${50 - (mouse_w - w) * 0.08}% ${50 - (mouse_h - h) * 0.08}%`;
-    const pos2 = `${50 - (mouse_w - w) * 0.04}% ${50 - (mouse_h - h) * 0.04}%`;
-    const pos3 = `${50 - (mouse_w - w) * 0.02}% ${50 - (mouse_h - h) * 0.02}%`;
-    const pos4 = `${50 - (mouse_w - w) * 0.01}% ${50 - (mouse_h - h) * 0.01}%`;
-    const final_position = `${pos1}, ${pos2}, ${pos3}, ${pos4}`;
-    el.style.backgroundPosition = final_position;
-  } 
+  const w = window.innerWidth/2;
+  const h = window.innerHeight/2;
+
+  let mouse_w;
+  let mouse_h;
+
+  if (e.type === 'touchstart' || e.type === 'touchmove') {
+    mouse_w = e.touches[0].clientX;
+    mouse_h = e.touches[0].clientY
+  } else {
+    mouse_w = e.clientX;
+    mouse_h = e.clientY;
+  }
+
+  const pos1 = `${50 - (mouse_w - w) * 0.08}% ${50 - (mouse_h - h) * 0.08}%`;
+  const pos2 = `${50 - (mouse_w - w) * 0.04}% ${50 - (mouse_h - h) * 0.04}%`;
+  const pos3 = `${50 - (mouse_w - w) * 0.02}% ${50 - (mouse_h - h) * 0.02}%`;
+  const pos4 = `${50 - (mouse_w - w) * 0.01}% ${50 - (mouse_h - h) * 0.01}%`;
+  const final_position = `${pos1}, ${pos2}, ${pos3}, ${pos4}`;
+  el.style.backgroundPosition = final_position;
 }
 
 const Anim1 = () => {
     return (
-      <div className="parallax container" onMouseMove={parallax}></div>
+      <div className="parallax container" onMouseMove={parallax} onTouchStart={parallax} onTouchMove={parallax}></div>
     )
 }
 
