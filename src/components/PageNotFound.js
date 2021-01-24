4f41e28b8bc6da0c22a85764ac082af0dc92a04a
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2021-01-24 14:18:48 +0100

    Update PageNotFound component

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import '../styles/PageNotFound.css';
 
 const PageNotFound = () => {
   return (
-    <div>
-      <h1>Page Not Found 404</h1>
+    <div className="page_error container">
+      <h2>Sorry, something went wrong... 404</h2>
+      <h3>You could see my
+        <a href="https://freefrogs.github.io/AboutMe/" target="_blank" rel="noreferrer"> portfolio page </a>
+        or color a geometric pet;-)
+      </h3>
+      <iframe title="page not found" src="https://freefrogs.github.io/EX_Canvas/"></iframe>
     </div>
   )
 }
